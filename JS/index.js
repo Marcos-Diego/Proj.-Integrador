f27175f0ab75c42f40d56a2d6c695b2993a11c30
M <diegomarcos12122006@gmail.com>
2025-10-16 17:06:43 -0300

    menu e banner

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,22 @@
 //inicio do header
-let li = document.getElementsByTagName("li");
-let lMenu = document.getElementsByTagName("ul");
-const corpo = document.body.children;
+const pai = document.getElementsByTagName("ul");
 
-lMenu = lMenu[0].children;
+let filhos = pai[0].children;
 
-for (let x = 0; x < lMenu.length; x++) {
-    li[x].classList.add("hover:text-gray-950", "text-gray-700", "mb-[40px]", "pb-[5px]", "pl-[8px]", "w-[100%]", 
-        "shadow-xl", "border-b-2");
-    if (x == 0) {
-        li[x].classList.add("mt-[30px]")
-    };
-};
+for (let x = 0; x < filhos.length; x++) {
+    let netos = filhos[x].children; //o netos são o p e hr
+    console.log(netos[0])
+    for (let y = 0; y < netos.length; y++) {
+        if (y == 0) {
+            netos[y].classList.add("hover:text-gray-950", "text-gray-700", "mb-[40px]", "pb-[5px]", "pl-[8px]", "w-[100%]");
+            if (x == 0) { netos[y].classList.add("mt-[30px]") };
+        }
+        else if (y == 1) {
+            netos[y].classList.add("width=[50%]");
+        }
+    }
+}
+
 
 //fim do header
 //inicio do nav
@@ -28,25 +33,26 @@ menuOpen.addEventListener("click", () => {
 )
 //fim do nav
 //inicio do banner
-    //inicio de img
+//inicio de img
 const banner = document.getElementById("banner")
 let nextBanner = banner.lastElementChild;
 
 const listaIMG = ["img/Amantino_Camara_Logo.png", "img/git_imagem.jpg"];
 let imgMAX = listaIMG.length;
 console.log(imgMAX)
-    
+
 function tempo() {
     if (nextBanner == "bg-[url(img/Amantino_Camara_Logo.png)]")
-    setInterval(start, 5000);};
-    function start() {
+        setInterval(start, 5000);
+};
+function start() {
 
-    }
-    
+}
+
 tempo();
-    
-    //fim de img
-    //inicio de botoes e suas funçoes
+
+//fim de img
+//inicio de botoes e suas funçoes
 const bannerLeft = document.getElementById("banner-left");
 bannerLeft.addEventListener("click", () => {
     tempo()
@@ -55,7 +61,7 @@ const bannerRight = document.getElementById("banner-right");
 bannerRight.addEventListener("click", () => {
     tempo()
 
- });
-    //fim de botoes e suas funçoes
+});
+//fim de botoes e suas funçoes
 
 //fim do banner
